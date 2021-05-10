326809fa7b67b0c8750615a003f738a3e93fde3e
Kavya <kavya@Kavyas-iMac.local>
2021-05-10 20:47:45 +0530

    Starter

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="">
+           <h1>Heelo</h1>
+           <div></div>
+        </header>
+    )
+}
+
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Starter - Home</title>
+        
+      </Head>
+
+      <main className="main">
+        <Header />
+      Content
+      <Footer />
+      </main>
+
+      <footer className="footer">
+       
+      </footer>
+    </div>
+  )
+}
