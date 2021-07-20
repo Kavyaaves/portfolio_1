620c446e95e301dc336015173fde65a94b5e4fa3
Kavya Mm <kavyaaves2019@gmail.com>
2021-07-21 01:29:12 +0530

    NavBar Error 3

--- components/NavBar.js
+++ components/NavBar.js
@@ -90,7 +90,7 @@ function NavBar() {
 			<div
 				className={
 					isOpen
-						? 'translate-x-full transition-all ease-in p-5 duration-300 absolute w-screen top-0 z-10 block md:hidden h-screen flex-shrink-0 bg-primary'
+						? 'absolute md:relative translate-x-full transition-all ease-in p-5 duration-300 w-screen top-0 z-20 flex md:hidden h-screen left-0 flex-shrink-0 bg-primary'
 						: 'hidden'
 				}>
 				<div
