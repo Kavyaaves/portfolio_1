9eab5593f03b2e03ee7e535a709af9cb4b12e5dd
Kavya Mm <kavyaaves2019@gmail.com>
2021-08-01 01:42:02 +0530

    Changed api to web

--- pages/gallery.js
+++ pages/gallery.js
@@ -6,7 +6,135 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import ImagePreview from '../components/ImageGrid';
 import Modal from '../components/Modal';
-function gallery({ all }) {
+function gallery() {
+	const marineImages = [
+		[{
+			id: 1,
+			name: 'Acanthophora spicifera',
+			url: '/marine/Acanthophora spicifera.JPG',
+			caption: 'imagae 1',
+		},
+		{
+
+			id: 2,
+			name: 'Avrainvillea erecta',
+			url: '/marine/Avrainvillea erecta.jpg',
+			caption: 'imagae 1',
+		},
+		{
+			id: 3,
+
+			name: 'Boergesenia forbesii',
+			url: '/marine/Boergesenia forbesii.jpg',
+			caption: 'imagae 1',
+		}],
+		[{
+			id: 4,
+
+			name: 'Botryocladia sp',
+			url: '/marine/Botryocladia sp.JPG',
+			caption: 'imagae 1',
+		},
+		{
+			id: 5,
+
+			name: 'Caulerpa cupressoides',
+			url: '/marine/Caulerpa cupressoides.jpg',
+			caption: 'imagae 1',
+		},
+		{
+			id: 6,
+
+			name: 'Caulerpa racemosa',
+			url: '/marine/Caulerpa racemosa.jpg',
+			caption: 'imagae 1',
+		}],
+		[{
+			id: 8,
+
+			name: 'Caulerpa scalpelliformis',
+			url: '/marine/Caulerpa scalpelliformis.JPG',
+			caption: 'imagae 1',
+		},
+		{
+			id: 9,
+
+			name: 'Caulerpa taxifolia',
+			url: '/marine/Caulerpa taxifolia.jpg',
+			caption: 'imagae 1',
+		},
+		{
+			id: 10,
+
+			name: 'Caulerpa',
+			url: '/marine/Caulerpa.jpg',
+			caption: 'imagae 1',
+		}],
+	];
+	const freshWaterImages = [[{
+		id: 7,
+		name: 'Colpomenia sinuosa',
+		url: '/marine/Colpomenia sinuosa.jpg',
+		caption: 'imagae 1',
+	},
+	{
+
+		id: 2,
+		name: 'Dictyosphaeria cavernosa',
+		url: '/marine/Dictyosphaeria cavernosa.jpg',
+		caption: 'imagae 1',
+	},
+	{
+		id: 3,
+
+		name: 'Halimeda bearing Gametangia',
+		url: '/marine/Halimeda bearing Gametangia.jpg',
+		caption: 'imagae 1',
+	}],
+	[{
+		id: 4,
+
+		name: 'Liagora sp',
+		url: '/marine/Liagora sp.jpg',
+		caption: 'imagae 1',
+	},
+	{
+		id: 5,
+
+		name: 'Lobophora variegata',
+		url: '/marine/Lobophora variegata.jpg',
+		caption: 'imagae 1',
+	},
+	{
+		id: 6,
+
+		name: 'Martensia',
+		url: '/marine/Martensia_114654.jpg',
+		caption: 'imagae 1',
+	}],
+	[{
+		id: 8,
+
+		name: 'Halimeda',
+		url: '/marine/Halimeda.jpg',
+		caption: 'imagae 1',
+	},
+	{
+		id: 9,
+
+		name: 'Spatoglossum variabile',
+		url: '/marine/Spatoglossum variabile.jpg',
+		caption: 'imagae 1',
+	},
+	{
+		id: 10,
+
+		name: 'Ulva sp',
+		url: '/marine/Ulva sp.jpg',
+		caption: 'imagae 1',
+	}]]
+
+	const all = { marine: marineImages, freshWater: freshWaterImages }
 	const [allImages, setAllImages] = useState(all)
 	const [selectedImg, setSelectedImg] = useState('')
 	const [isVisible, setIsVisible] = useState(false);
@@ -93,13 +221,15 @@ function gallery({ all }) {
 
 export default gallery;
 
-export async function getStaticProps() {
-	const results = await fetch('https://bakthavachalam-babu.vercel.app/api/gallery');
-	const all = await results.json()
-	console.log("getsta", all)
-	return {
-		props: {
-			all,
-		}
-	}
-}
+// export async function getStaticProps() {
+// 	const results = await fetch('https://bakthavachalam-babu.vercel.app/api/gallery');
+// 	console.log(results)
+// 	const all = await results.json()
+// 	console.log(all)
+// 	console.log("getsta", all)
+// 	return {
+// 		props: {
+// 			all,
+// 		}
+// 	}
+// }
