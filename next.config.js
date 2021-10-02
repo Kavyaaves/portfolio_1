10d3903ea75512557649cf123d604cabd54a6ac0
Kavya Mm <kavya.murali@techdome.net.in>
2021-10-03 00:23:55 +0530

    Test 6

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
     compress: true,
     productionBrowserSourceMaps: true,
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    output: {
+                        comments: false
+                    },
+                },
+                extractComments: false
+            }),
+        ]
+    }
 }
