d50cf4f9729a7f7513ac93c98088a43cdc92512d
Kavya Mm <kavya.murali@techdome.net.in>
2021-10-03 00:15:17 +0530

    Test 5

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
-const withPWA = require("next-pwa");
-const TerserPlugin = require('terser-webpack-plugin')
-
-module.exports = withPWA({
-    pwa: {
-        dest: "public",
-        register: true,
-        mode: 'production',
-        skipWaiting: true,
-    },
+module.exports = {
     compress: true,
-    webpack5: false,
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    output: {
-                        comments: false
-                    },
-                },
-                extractComments: false
-            }),
-        ]
-    }
-});
+    productionBrowserSourceMaps: true,
+}
