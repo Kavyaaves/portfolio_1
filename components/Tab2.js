7389a1db16a618756c20e75440a1de722f95867a
Kavya Mm <kavya.murali@techdome.net.in>
2021-10-03 15:11:30 +0530

    Syllabus gap changed

--- components/Tab2.js
+++ components/Tab2.js
@@ -2,7 +2,7 @@ function Tab2({ syllabus }) {
     return (
 
         <div className="w-full md:flex pl-8">
-            <div className="md:w-1/2 pr-5" >
+            <div className="md:w-1/2 md:mr-10" >
                 <p className="text-lg text-yellow-500 p-5 pl-0 font-semibold">B. Sc</p>
                 {syllabus.bsc.map((d, i) => {
                     return (
@@ -15,7 +15,7 @@ function Tab2({ syllabus }) {
                     )
                 })}
             </div >
-            <div className="md:w-1/2 pl-5">
+            <div className="md:w-1/2 md:ml-10">
                 <p className="text-lg text-yellow-500 p-5 pl-0 font-semibold">M. Sc</p>
                 {syllabus.msc.map((d, i) => {
                     return (
