0e4e720e38edd57cdceecf10e026975732238b90
Kavya <kavya.murali@techdome.net.in>
2022-07-24 16:57:17 +0530

    Renamed images

--- pages/freshwater-algae/[category]/[name].js
+++ pages/freshwater-algae/[category]/[name].js
@@ -7,7 +7,7 @@ function freshwaterAlgaeDetail({ data }) {
     return (
         <>
             <Head>
-                <title>{data ? data.name : ''}Freshwater Algae - Bakthavachalam Babu</title>
+                <title>{data ? data.name : ''} - Freshwater Algae - Bakthavachalam Babu</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta charSet="utf-8" />
                     <meta name="google" content="notranslate" />
